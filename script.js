f0cbfdd37ab68ad87fa3de6f97d1d01c2f71337d
Sandypie7878 <162807930+Sandypie7878@users.noreply.github.com>
2024-12-25 15:23:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('clickButton').addEventListener('click', function() {
+  const messageElement = document.getElementById('dynamicMessage');
+  messageElement.innerHTML = 'Thank you for choosing Delicious Dishes! Start your order now and enjoy the best food in town!';
+});
